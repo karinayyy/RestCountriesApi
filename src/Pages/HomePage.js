62653c5bc9baa3b0530e3f6d727fcc56c33f7928
karpenko-karina <karinakarpenko.k@gmail.com>
2022-12-31 13:48:00 +0200

    home page

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,16 @@
 import React from "react"
 import Navbar from "../Components/Navbar";
+import {GrNext} from "react-icons/gr";
+import {Link} from "react-router-dom";
 
 function HomePage() {
     return (
         <>
             <Navbar />
-            <h1>Homeeee</h1>
+            <h1>Home</h1>
+            <Link style={{color: "black"}} to={`/`}>
+                Learn geografy
+            </Link>
         </>
     )
 }
